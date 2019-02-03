78e9d49f19fdfbdd616cfc7586768479ec50266b
mondara1234 <40348446+mondara1234@users.noreply.github.com>
2019-02-04 01:41:01 +0700

    Install_camara&&imagePicker

--- android/app/src/main/java/com/healthytracker/MainApplication.java
+++ android/app/src/main/java/com/healthytracker/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.RNFetchBlob.RNFetchBlobPackage;
+import org.reactnative.camera.RNCameraPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
           new RNFetchBlobPackage(),
+          new RNCameraPackage(),
           new ImagePickerPackage(),
           new SplashScreenReactPackage(),
           new RNI18nPackage()
