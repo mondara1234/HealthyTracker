d4c474c80876d5d05a99ba77842745edeb591c64
mondara1234 <40348446+mondara1234@users.noreply.github.com>
2019-02-25 06:54:50 +0700

    Update Trans.js

--- src/features/common/containers/Trans.js
+++ src/features/common/containers/Trans.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import I18n from 'react-native-i18n';
 import th from '../../../i18n/locales/th';
 import en from '../../../i18n/locales/en';
@@ -11,7 +10,7 @@ import CommonText from '../components/CommonText';
 I18n.fallbacks = true;
 I18n.translations = {th, en};
 I18n.defaultLocale = DEFAULT_LOCALE;
-//I18n.locale = 'en';
+
 class Trans extends React.PureComponent {
     static tran(key, replace) {
         return I18n.t(key, replace);
@@ -27,20 +26,11 @@ class Trans extends React.PureComponent {
     };
 
     render() {
-        const {user} = this.props.Users ;
-        const Language = user.map((data) => {return data.Language});
-        I18n.locale = `${Language}`;
+
         return <CommonText {...this.props} text={I18n.t(this.props.t, this.props.replace)} />
     }
 }
 
-function mapStateToProps(state) {
-    return{
-        Users: state.dataUser
-    };
-}
 
-export default connect(
-    mapStateToProps,
-)(Trans);
+export default Trans;
 
