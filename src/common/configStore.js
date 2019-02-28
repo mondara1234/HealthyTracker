af9739c7a705a296c79f6d736420339ad75dfd24
mondara1234 <40348446+mondara1234@users.noreply.github.com>
2019-02-28 21:30:19 +0700

    Revert "Update configStore.js"
    This reverts commit 4014f19f9bcb049198948a1d85f915c4e47a37c7.

--- src/common/configStore.js
+++ src/common/configStore.js
@@ -1,14 +1,21 @@
 import createSagaMiddleware from "redux-saga";
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import rootReducer from './rootReducer';
 import rootSaga from './rootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const Middleware = applyMiddleware(sagaMiddleware, thunk);
-const store = createStore(rootReducer, Middleware);
+// แสดง ประเภท Actions ที่เกิดขึ้น
+const showLog = (store) => (next) => (action) => {
+    console.log("Log Action", action);
+    next(action);
+};
 
+const store = createStore(combineReducers
+    ({
+        ...rootReducer
+    }),{}, compose(applyMiddleware(sagaMiddleware, showLog))
+);
 // run the saga
 sagaMiddleware.run(rootSaga);
 
